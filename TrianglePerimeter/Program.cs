58fe7dfdfb252cd4b3145f7ca40e92a6ada806c9
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 21:57:51 +0300

    Initial commit

---
+++ TrianglePerimeter/Program.cs
@@ -0,0 +1,13 @@
+﻿class TrianglePerimeter
+
+{
+    static void Main(string[] args)
+
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b=int.Parse(Console.ReadLine());
+        int c=int.Parse(Console.ReadLine());
+        int perimeter = a + b + c;
+        Console.WriteLine(perimeter);
+    }
+}
